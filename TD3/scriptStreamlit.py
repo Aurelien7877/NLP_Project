59b40bee2152ee662b0a04d9cf8a3ffc6802eff4
Aurelien Pouxviel <aurelien.pouxviel@edu.devinci.fr>
2023-10-20 19:20:24 +0200

    fix

--- TD3/scriptStreamlit.py
+++ TD3/scriptStreamlit.py
@@ -5,8 +5,29 @@ from sklearn.metrics.pairwise import linear_kernel
 import os
 import sys
 import numpy as np
-sys.path.insert(0,'C:/Users/aurel/OneDrive - De Vinci/ONE DRIVE PC/A5/NLP/TD3/loadCNN.py')
-from loadCNN import loadCNN
+#sys.path.insert(0,'C:/Users/aurel/OneDrive - De Vinci/ONE DRIVE PC/A5/NLP/TD3/loadCNN.py')
+
+import pickle
+
+def loadCNN():
+	file = open("./CNNArticles",'rb')
+	articles = pickle.load(file)
+	file = open("./CNNGold",'rb')
+	abstracts = pickle.load(file)
+
+	articlesCl = []  
+	for article in articles:
+		articlesCl.append(article.replace("”", "").rstrip("\n"))
+	articles = articlesCl
+	  
+	articlesCl = []  
+	for article in abstracts:
+		articlesCl.append(article.replace("”", "").rstrip("\n"))
+	abstracts = articlesCl
+    
+	return articles, abstracts
+
+articles, abstracts = loadCNN()
 
 
 articles, abstracts = loadCNN()
