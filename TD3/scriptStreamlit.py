8d60db316e81e16640071479d197c91071d14054
Aurelien Pouxviel <aurelien.pouxviel@edu.devinci.fr>
2023-10-24 14:31:28 +0200

    smoother

--- TD3/scriptStreamlit.py
+++ TD3/scriptStreamlit.py
@@ -40,23 +40,30 @@ tfidf_vectorizer = TfidfVectorizer()
 article_tfidf_matrix = tfidf_vectorizer.fit_transform(articles)
 summary_tfidf_matrix = tfidf_vectorizer.transform(abstracts)
 
-st.title("WP3: Aurelien Pouxviel: Article Similarity App ")
+# Title with colored background and special font
+st.markdown(
+    "<h1 style='text-align: center; background-color: #F63366; color: white;"
+    "font-size: 32px; padding: 10px; border-radius: 10px;'>WP3: Aurelien Pouxviel: Article Similarity App</h1>",
+    unsafe_allow_html=True,
+)
 
 user_input = st.text_input("Enter some words to find similar articles:")
 
-if user_input:
-    st.text("Finding similar articles... ⏳")
-    
-    user_input_vector = tfidf_vectorizer.transform([user_input])
-    user_cosine_similarities = linear_kernel(user_input_vector, article_tfidf_matrix)
-    
-    # Find the top 2 similar articles
-    most_similar_article_indices = np.argsort(user_cosine_similarities[0])[-2:][::-1]
-    
-    st.subheader("Top 2💎​ Most Similar Articles:")
-    
-    for i, index in enumerate(most_similar_article_indices):
-        st.subheader(f"Article {i + 1} (Similarity Score: {user_cosine_similarities[0][index]:.2f}):")
-        st.write(articles[index])
+# Button to trigger the action
+if st.button("Find Similar Articles"):
+    if user_input:
+        # Loading bar
+        with st.spinner("Finding similar articles..."):
+            user_input_vector = tfidf_vectorizer.transform([user_input])
+            user_cosine_similarities = linear_kernel(user_input_vector, article_tfidf_matrix)
 
-    st.text("Search completed! ✅")
+            # Find the top 2 similar articles
+            most_similar_article_indices = np.argsort(user_cosine_similarities[0])[-2:][::-1]
+
+        st.subheader("Top 2 Most Similar Articles:")
+
+        for i, index in enumerate(most_similar_article_indices):
+            st.subheader(f"Article {i + 1} (Similarity Score: {user_cosine_similarities[0][index]:.2f}):")
+            st.write(articles[index])
+
+        st.success("Search completed! ✅")
